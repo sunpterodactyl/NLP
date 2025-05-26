356570f69bd00dc21e968e686d53cd5d10453d0b
sunpterodactyl <inforsunpteranadon@gmail.com>
2025-05-26 13:46:17 +0800

    Remove redundant test

--- VideoToPPT/app/commands/save_url.py
+++ VideoToPPT/app/commands/save_url.py
@@ -15,15 +15,9 @@ class IndexCommand(Command):
             self.urls = [self.urls]
         else:
             pass
-        
+
         for url in self.urls:
             id = get_video_id(url)
             print(f"Successfully retrieved id: {id}")
             index_documents_to_chroma(id, self.session_id)
             print(f"Successfully indexed {id} in session {self.session_id}")
-
-SESSION = "5aada9c7-05a4-4f6e-aa74-5ae4bb688178"
-URL = "https://www.youtube.com/watch?v=3kRB2TXewus"
-curr = IndexCommand(URL, SESSION)
-print(curr.execute())
-
